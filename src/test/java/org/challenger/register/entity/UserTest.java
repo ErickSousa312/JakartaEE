0b62d7f7aaff45986cf4b3b1f254f17898f41301
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-07-18 23:57:40 -0300

    created tests dependencies

---
+++ src/test/java/org/challenger/register/entity/UserTest.java
@@ -0,0 +1,32 @@
+package org.challenger.register.entity;
+
+import org.challenger.register.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getUsername() {
+        User user = new User();
+        user.setUsername("test");
+        assertThat(user.getUsername()).isEqualTo("test");
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
